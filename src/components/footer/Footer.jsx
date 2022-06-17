41c3ed1aa6354a6a7b1e0e653c0a9ab3e7799e4c
zeredogo <zeredogo@gmail.com>
2022-06-17 14:34:53 -0700

    Added a change

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -21,12 +21,12 @@ const Footer = () => {
                 <li className="fListItem">Hotels</li>
             </ul>
             <ul className="fList">
-                <li className="fListItem">Countries</li>
-                <li className="fListItem">Regions</li>
-                <li className="fListItem">Cities</li>
-                <li className="fListItem">Districts</li>
+                <li className="fListItem">Dogo</li>
+                <li className="fListItem">Plateau</li>
+                <li className="fListItem">Jos</li>
+                <li className="fListItem">Tudun Wada</li>
                 <li className="fListItem">Airports</li>
-                <li className="fListItem">Hotels</li>
+                <li className="fListItem">K-Rocks Hotel</li>
             </ul>
             <ul className="fList">
                 <li className="fListItem">Countries</li>
